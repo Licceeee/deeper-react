0a2846f6caf122d6ffe40ca867122826c90c014a
Alicia Schonefeld <schonefeld.dev@gmail.com>
2021-07-22 19:00:19 +0200

    split title in separated component

---
+++ src/components/TitleLG/TitleLG.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./TitleLG.css";
+
+const TitleLG = () => {
+  return (
+    <div className="shape">
+      <h1 className="page-title">DEEPER</h1>
+    </div>
+  );
+};
+
+export default TitleLG;
