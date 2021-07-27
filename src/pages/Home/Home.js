27bfa9db703500445cb8dc0a36fc197446205501
Alicia Schonefeld <schonefeld.dev@gmail.com>
2021-07-27 14:37:13 +0200

    update home page and css

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,18 +3,19 @@ import "./Home.css";
 import image from "../../assets/img/cards-img.svg";
 import Button from "../../components/Button/Button";
 import { Link } from "react-router-dom";
+import Triangle from "../../components/Triangle/Triangle";
 
 const Home = () => {
   return (
     <div className="main-container mx-0">
       <div className="row col-12 first-section p-0 m-0">
-        <div className="col-12 col-xl-6 inner-container p-0 m-0">
-          <img src={image} className="img"></img>
+        <div className="col-12 col-xl-6 inner-container p-0 m-0 first">
+          <img src={image} className="img" alt="card deck"></img>
         </div>
 
         <div
           className="row col-12 col-xl-6 inner-container p-0 m-0 d-flex flex-column 
-        align-content-center"
+        align-content-center second"
         >
           <div className="text">
             <h2 className="pb-5"> Question game</h2>
@@ -25,11 +26,14 @@ const Home = () => {
             </p>
           </div>
         </div>
-      </div>
-      <div className="section-choose-way-to-play">
-        <Link to="/categories">
-          <Button text="Select a category" cssClass="btn home-button my-5" />
-        </Link>
+        <div className="section-choose-way-to-play">
+          <Link to="/categories">
+            <Button text="Select a category" cssClass="btn home-button my-5" />
+          </Link>
+        </div>
+        {/* <div className="section-choose-way-to-play">
+          <Triangle />
+        </div> */}
       </div>
     </div>
   );
