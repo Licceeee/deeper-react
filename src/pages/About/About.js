dbc2b61abbef409cb35c08b5256804def8349d43
Alicia Schonefeld <schonefeld.dev@gmail.com>
2021-07-28 17:55:40 +0200

    add about page

---
+++ src/pages/About/About.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+import "./About.css";
+import HeaderSM from "../../components/HeaderSM/HeaderSM";
+
+const About = () => {
+  return (
+    <Fragment>
+      <HeaderSM />
+      <div className="page-size"></div>
+    </Fragment>
+  );
+};
+
+export default About;
