9aa3c2ce23f8012b3369720e0a1aade36dd043e3
Alicia Schonefeld <schonefeld.dev@gmail.com>
2021-07-28 18:03:32 +0200

    add rules page

---
+++ src/pages/Rules/Rules.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+import "./Rules.css";
+import HeaderSM from "../../components/HeaderSM/HeaderSM";
+
+const Rules = () => {
+  return (
+    <Fragment>
+      <HeaderSM />
+      <div className="page-size"></div>
+    </Fragment>
+  );
+};
+
+export default Rules;
