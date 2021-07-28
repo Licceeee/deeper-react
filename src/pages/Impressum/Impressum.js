62192c845b3cc2dbd53efe5c82b0080959abb3bc
Alicia Schonefeld <schonefeld.dev@gmail.com>
2021-07-28 18:03:11 +0200

    add impressum page

---
+++ src/pages/Impressum/Impressum.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from "react";
+import "./Impressum.css";
+import HeaderSM from "../../components/HeaderSM/HeaderSM";
+
+const Impressum = () => {
+  return (
+    <Fragment>
+      <HeaderSM />
+      <div className="page-size"></div>
+    </Fragment>
+  );
+};
+
+export default Impressum;
+
