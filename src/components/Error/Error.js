4dfc3193734a4db6162eb99ae1d566552c6bb60a
Alicia Schonefeld <schonefeld.dev@gmail.com>
2021-10-03 12:36:25 +0200

    add error component

---
+++ src/components/Error/Error.js
@@ -0,0 +1,17 @@
+import React, { Fragment } from "react";
+import "./Error.css";
+
+const Error = ({ message }) => {
+  return (
+    <div className="text-center push-down">
+      <div className="spinner-error-content">
+        <div className="row d-flex justify-content-center">
+          <h4 className="center mb-3">Error ... </h4>
+          <p className="center mb-3">{message} </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Error;
