b5a2a1ca0c0df3cc7618885a26ccc32676ccebb7
Alicia Schonefeld <schonefeld.dev@gmail.com>
2022-06-19 23:11:42 +0200

    update footer style

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,47 +5,37 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <footer>
-      <div className="container flex col-12">
+      <div className="row col-12">
         <div className="section border-right col-12 col-md-4">
           <div className="inner-footer-container">
-            {/* <Link to="/rules" >
-              <p>Rules</p>
-            </Link> */}
-            <p>Download die App (Coming soon...)</p>
-            <p>Rules (Coming soon...) </p>
-
+            <Link to="#">Download die App (Coming soon...)</Link>
+            <Link to="#">Rules (Coming soon...) </Link>
           </div>
         </div>
 
         <div className="section border-right col-12 col-md-4">
           <div className="inner-footer-container">
             <Link to="/about">About Us</Link>
-            <Link to="/contact">
-              <p>Contact Us</p>
-            </Link>
+            <Link to="/contact">Contact Us</Link>
           </div>
         </div>
 
         <div className="section col-12 col-md-4">
           <div className="inner-footer-container">
-            <p>
-              <a
-                href="https://www.linkedin.com/in/alicia-schonefeld/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                LinkedIn
-              </a>
-            </p>
-            <p>
-              <a
-                href="https://github.com/Licceeee"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                GitHub
-              </a>
-            </p>
+            <a
+              href="https://www.linkedin.com/in/alicia-schonefeld/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              LinkedIn
+            </a>
+            <a
+              href="https://github.com/Licceeee"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              GitHub
+            </a>
           </div>
         </div>
       </div>
