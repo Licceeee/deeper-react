2b6c9d4e90c9475da732026beb42b523573cc914
Alicia Schonefeld <schonefeld.dev@gmail.com>
2022-07-01 17:22:53 +0200

    add build

---
+++ build/precache-manifest.875c01466b4207bfc35f0d8edbfe5906.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "6724004391540be542c7",
+    "url": "/static/css/main.6735fb5e.chunk.css"
+  },
+  {
+    "revision": "6724004391540be542c7",
+    "url": "/static/js/main.b68d0910.chunk.js"
+  },
+  {
+    "revision": "085f9a419e02f8051f99",
+    "url": "/static/js/runtime~main.32cc1d31.js"
+  },
+  {
+    "revision": "bc88d286ac6102c8ede6",
+    "url": "/static/js/2.77a751ef.chunk.js"
+  },
+  {
+    "revision": "fb68ece5a0e000383d75",
+    "url": "/static/js/3.7d79ecc6.chunk.js"
+  },
+  {
+    "revision": "eff07a3b369a7d2d6ccdbe281b44e890",
+    "url": "/static/media/cards-img.eff07a3b.svg"
+  },
+  {
+    "revision": "f985de2b39ea60e6102ad9e21c555bb9",
+    "url": "/static/media/arrow-down2.f985de2b.svg"
+  },
+  {
+    "revision": "db5ffedcef425b11f08b5a66c1943d54",
+    "url": "/static/media/circle-line.db5ffedc.svg"
+  },
+  {
+    "revision": "094581ceda77d2aeb7b223a188d74af2",
+    "url": "/static/media/lic.094581ce.jpeg"
+  },
+  {
+    "revision": "37a6add2b9fc7923252acf7d7042d22a",
+    "url": "/static/media/fil.37a6add2.jpeg"
+  },
+  {
+    "revision": "091c85b40d6a9dfd305ab6c85a566831",
+    "url": "/static/media/Sen-ExtraBold.091c85b4.ttf"
+  },
+  {
+    "revision": "f7e19def91dc7d0a145dcf8fcec4cc8a",
+    "url": "/static/media/Sen-Bold.f7e19def.ttf"
+  },
+  {
+    "revision": "c92d05ebbdb4c63d69dc1c119a69e0ee",
+    "url": "/static/media/Sen-Regular.c92d05eb.ttf"
+  },
+  {
+    "revision": "e1619da5fde46c5949443ee08a3b94fe",
+    "url": "/index.html"
+  }
+];
